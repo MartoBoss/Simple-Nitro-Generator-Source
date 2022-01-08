544453041951a44adaa7fa519e15ec770240df84
MartoBoss <77393924+MartoBoss@users.noreply.github.com>
2022-01-08 13:10:44 +0200

    Add files via upload

---
+++ n-gen-by-$MartoBossX.py
@@ -0,0 +1,13 @@
+import random, string
+
+amount = int(input('Amount of nitro codes to generate: '))
+value = 1
+while value <= amount:
+	code = "https://discord.gift/" + ('').join(
+	    random.choices(string.ascii_letters + string.digits, k=16))
+	f = open('Codes by LuckAxx.txt', "a+")
+	f.write(f'{code}\n')
+	f.close()
+	print(f'{code}')
+	value += 1
+print("\n\n\nJoin in our discord https://discord.gg/AsN6tY3KMr")
